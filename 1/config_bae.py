f28cd1691be8e1a9734941d238a23d79731dd4e6
Tavern <windack@qq.com>
2013-09-16 17:29:35 +0800

    test

--- 1/config_bae.py
+++ 1/config_bae.py
@@ -7,8 +7,8 @@ PER_PAGE = 10
 
 # configuration mysql
 SQLALCHEMY_POOL_RECYCLE = 10
-SQLALCHEMY_DATABASE_URI = "sqlite:///db/foo.db"
-# SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@%s:%s/%s?charset=utf8' % (const.MYSQL_USER, const.MYSQL_PASS, const.MYSQL_HOST,
-#                                                                    int(const.MYSQL_PORT), 'wbreGfHAdijPKMJFqwns')
+# SQLALCHEMY_DATABASE_URI = "sqlite:///db/foo.db"
+SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@%s:%s/%s?charset=utf8' % (const.MYSQL_USER, const.MYSQL_PASS, const.MYSQL_HOST,
+                                                                   int(const.MYSQL_PORT), 'wbreGfHAdijPKMJFqwns')
 AK = 'z9NR4maoM1CNEUZ3BRvrv2GK'
 SK = 'IRxtOZy9Fxq5s68zHdWyqt7ax2US3U6D'
