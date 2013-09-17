2183ee0074d38b129ca79f7b12144959079d3272
Tavern <windack@qq.com>
2013-09-17 21:22:52 +0800

    improved

--- 1/index.py
+++ 1/index.py
@@ -1,29 +1,11 @@
 #-*- coding:utf-8 -*-
 
-# from bae.core.wsgi import WSGIApplication
-# # from config.engine import app, factory, db
-# from test import app
-# from bae.api import logging
-#
-#
-# # factory.Register_Config(conf='config_bae')
-# # db.create_all()
-#
-# application = WSGIApplication(app)
-
-from flask import Flask, g, request
-
-app = Flask(__name__)
-app.debug = True
-
-@app.route('/')
-def hello():
-    return "Hello, world! - Flask\n"
-
-
-@app.route('/test')
-def test():
-    return "test, world! - Flask\n"
-
 from bae.core.wsgi import WSGIApplication
+from config.engine import app, factory, db
+from bae.api import logging
+
+
+factory.Register_Config(conf='config_bae')
+db.create_all()
+
 application = WSGIApplication(app)
--- 1/local.py
+++ 1/local.py
@@ -1,11 +1,7 @@
-# from config import engine
-#
-# engine.factory.Register_Config()
-# engine.db.create_all()
-# app = engine.app
-# if __name__ == '__main__':
-#     engine.app.run('0.0.0.0', 8080)
+from config import engine
 
-from test import app
+engine.factory.Register_Config()
+engine.db.create_all()
+app = engine.app
 if __name__ == '__main__':
-    app.run('0.0.0.0', 3000)
+    engine.app.run('0.0.0.0', 8080)
