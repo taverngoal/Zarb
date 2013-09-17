2d0754194b6a2194577f1a5fd89c8d51212d9713
Tavern <windack@qq.com>
2013-09-17 21:15:56 +0800

    improved

--- 1/test.py
+++ 1/test.py
@@ -3,18 +3,17 @@
 __author__ = 'Tavern'
 
 from flask import Flask
-from config.login import login_manager
-from ext.flask_login import login_required
+# from config.login import login_manager
+# from ext.flask_login import login_required
 
 
 app = Flask(__name__)
-app.debug = True
-app.secret_key = "!@#$%^&*()(*&^%#$@!#$%^&*()(*&^%$#@"
-login_manager.init_app(app)
+# app.debug = True
+# app.secret_key = "!@#$%^&*()(*&^%#$@!#$%^&*()(*&^%$#@"
+# login_manager.init_app(app)
 
 
 @app.route('/')
-@login_required
 def index():
     return 'haha'
 
